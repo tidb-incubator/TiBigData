3a6fb05eb5fed4e70decdcd20424a1729d638027
marsishandsome <marsishandsome@gmail.com>
2021-01-28 16:03:25 +0800

    support multi version of flink

--- .ci/test.sh
+++ .ci/test.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 
 export TIDB_URL="jdbc:tidb://127.0.0.1:4000?user=root&password="
 mvn clean test -am -pl jdbc
-mvn clean test -am -pl flink
+mvn clean test -am -pl flink/flink-1.12
 mvn clean test -am -pl prestodb
