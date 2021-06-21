be0a2faf9e70261647dccf19d109f379b22d412c
Xiaoguang Sun <sunxiaoguang@zhihu.com>
2021-06-21 21:38:27 +0800

    Always decode encoded terms in delta varint chunk format
    Signed-off-by: Xiaoguang Sun <sunxiaoguang@zhihu.com>

--- ticdc/src/main/java/io/tidb/bigdata/cdc/craft/CraftTermDictionary.java
+++ ticdc/src/main/java/io/tidb/bigdata/cdc/craft/CraftTermDictionary.java
@@ -1,6 +1,7 @@
 package io.tidb.bigdata.cdc.craft;
 
 import java.io.IOException;
+import java.util.function.Function;
 
 public class CraftTermDictionary {
   private final String[] terms;
@@ -18,35 +19,34 @@ public class CraftTermDictionary {
     return EMPTY_DICTIONARY;
   }
 
-  public String decode(int id) throws IOException {
+  public String decode(int id) {
     if (id >= this.terms.length || id < 0) {
       throw new IllegalArgumentException("Invalid term id: " + id);
     }
     return this.terms[id];
   }
 
-  public String decodeNullable(int id) throws IOException {
+  public String decodeNullable(int id) {
     if (id == -1) {
       return null;
     }
     return decode(id);
   }
 
-  public String[] decodeChunk(Codec codec, int elements) throws IOException {
-    long[] id = codec.decodeDeltaUvarintChunk(elements);
+  private String[] doDecodeChunk(Codec codec, int elements, Function<Integer, String> decoder) {
+    long[] id = codec.decodeDeltaVarintChunk(elements);
     String[] terms = new String[elements];
     for (int idx = 0; idx < elements; ++idx) {
-      terms[idx] = decode((int) id[idx]);
+      terms[idx] = decoder.apply((int) id[idx]);
     }
     return terms;
   }
 
+  public String[] decodeChunk(Codec codec, int elements) throws IOException {
+    return doDecodeChunk(codec, elements, this::decode);
+  }
+
   public String[] decodeNullableChunk(Codec codec, int elements) throws IOException {
-    long[] id = codec.decodeDeltaVarintChunk(elements);
-    String[] terms = new String[elements];
-    for (int idx = 0; idx < elements; ++idx) {
-      terms[idx] = decodeNullable((int) id[idx]);
-    }
-    return terms;
+    return doDecodeChunk(codec, elements, this::decodeNullable);
   }
 }
