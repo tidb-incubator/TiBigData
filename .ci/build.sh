811f63fd81f69c4bfebf61aef723b3adee3bbd5e
Xiaoguang Sun <sunxiaoguang@users.noreply.github.com>
2021-07-09 22:57:46 +0800

    Add flink-1.13 to ci build and test (#87)
    Signed-off-by: Xiaoguang Sun <sunxiaoguang@zhihu.com>

--- .ci/build.sh
+++ .ci/build.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 mvn clean compile -am -pl ticdc
 mvn clean compile -am -pl flink/flink-1.11
 mvn clean compile -am -pl flink/flink-1.12
+mvn clean compile -am -pl flink/flink-1.13
 mvn clean compile -am -pl mapreduce/mapreduce-base
 mvn clean compile -am -pl prestodb
 mvn clean compile -am -pl jdbc
