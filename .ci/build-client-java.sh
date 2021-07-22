5b47d399b1677ea1c43c1653029d5aab9da5a17c
Xiaoguang Sun <sunxiaoguang@zhihu.com>
2021-07-22 11:14:59 +0800

    Update tikv-client-java

--- .ci/build-client-java.sh
+++ .ci/build-client-java.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 git clone https://github.com/tikv/client-java.git
 cd client-java
-git reset --hard 309c521e9842ee9717ce12cd54513641fdb0b8eb
+git reset --hard faf0cb435d5d796692c4d986ad7478d87f3cf19a
 mvn install -DskipTests
 cd ..
 rm -Rf client-java
