91c375c2236255cb10ff2d3f3ca5e6351bc850ae
Kolbe Kegel <kolbe@pingcap.com>
2021-09-28 06:08:57 +0000

    Bumped .ci/build-client-java.sh to cover TLS support in tikv/client-java

--- .ci/build-client-java.sh
+++ .ci/build-client-java.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#/bin/bash
 
 set -x
 set -euo pipefail
 
 git clone https://github.com/tikv/client-java.git
 cd client-java
-git reset --hard 309c521e9842ee9717ce12cd54513641fdb0b8eb
+git reset --hard 28380512f3adacc1acb54daa381c4c71b1a1fa8f
 mvn install -DskipTests
 cd ..
 rm -Rf client-java
