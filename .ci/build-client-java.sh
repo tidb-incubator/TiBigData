7be7fe58b224673bb0f9a5a567150584491faf70
humengyu <hellohumengyu@gmail.com>
2022-01-04 11:45:15 +0800

    update build command

--- .ci/build-client-java.sh
+++ .ci/build-client-java.sh
@@ -3,9 +3,11 @@
 set -x
 set -euo pipefail
 
-git clone https://github.com/tikv/client-java.git
+if [ ! -d "client-java" ]; then
+  git clone https://github.com/tikv/client-java.git
+fi
 cd client-java
 git reset --hard 1405fe7c800ba319cf3b13c943939b0012e5b899
-mvn install -DskipTests
+mvn clean install -Dmaven.test.skip=true
 cd ..
 rm -Rf client-java
