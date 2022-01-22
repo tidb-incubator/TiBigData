f6ebc461f72fee35a13aebf4a14f07feeaf89ede
humengyu <hellohumengyu@gmail.com>
2022-01-22 17:27:14 +0800

    add License & build hive command

--- .ci/build.sh
+++ .ci/build.sh
@@ -12,6 +12,9 @@ mvn clean compile -am -pl flink/flink-1.13
 mvn clean compile -am -pl mapreduce/mapreduce-base
 mvn clean compile -am -pl prestodb
 mvn clean compile -am -pl jdbc
+mvn clean compile -am -pl hive/hive-2.2.0
+mvn clean compile -am -pl hive/hive-2.3.6
+mvn clean compile -am -pl hive/hive-3.1.2
 
 export JAVA_HOME=/home/jenkins/agent/lib/jdk-11.0.12
 mvn clean compile -am -pl prestosql
