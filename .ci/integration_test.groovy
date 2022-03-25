38aee9add63d8ab39836d51d9ba450197507f99c
yangxin <yangxin@pingcap.com>
2022-03-25 15:16:22 +0800

    fix

--- .ci/integration_test.groovy
+++ .ci/integration_test.groovy
@@ -126,10 +126,9 @@ def call(ghprbActualCommit, ghprbPullId, ghprbPullTitle, ghprbPullLink, ghprbPul
                                         bin/tikv-server --pd=127.0.0.1:2379 -s tikv --addr=0.0.0.0:20160 --advertise-addr=127.0.0.1:20160 --config=../.ci/config/tikv.toml &>tikv.log &
                                         sleep 10
                                         ps aux | grep '-server' || true
-                                        curl -s 127.0.0.1:2379/pd/api/v1/status
                                         bin/tidb-server --store=tikv --path="127.0.0.1:2379" --config=../.ci/config/tidb.toml &>tidb.log &
                                         sleep 60
-                                        cat _run/pd.log
+                                        cat pd.log
                                     """
 
                                 sh """
