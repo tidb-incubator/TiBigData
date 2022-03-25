4381402fe1c95c4c89113410b007f62a044a313e
yangxin <yangxin@pingcap.com>
2022-03-25 15:45:48 +0800

    fix

--- .ci/integration_test.groovy
+++ .ci/integration_test.groovy
@@ -78,8 +78,8 @@ def call(ghprbActualCommit, ghprbPullId, ghprbPullTitle, ghprbPullLink, ghprbPul
                         container("java") {
                             unstash 'tibigdata'
 
-
-                            sh "ls -al /home/jenkins/agent/git/tibigdata"
+                            sh "ls -al"
+                            sh "pwd"
                             dir("/home/jenkins/agent/lib") {
                                 sh "curl https://download.pingcap.org/jdk-11.0.12_linux-x64_bin.tar.gz | tar xz"
 
@@ -94,27 +94,28 @@ def call(ghprbActualCommit, ghprbPullId, ghprbPullTitle, ghprbPullLink, ghprbPul
                                         curl -sL \$archive_url | tar -zx -C /maven
                                         """, returnStatus: true)
                             }
+                            try {
+                                dir("_run") {
+                                    sh "pwd"
+                                    sh "rm -rf *"
 
-                            dir("/home/jenkins/agent/git/tibigdata/_run") {
-                                sh "rm -rf *"
+                                    // tidb
+                                    def tidb_sha1 = sh(returnStdout: true, script: "curl ${FILE_SERVER_URL}/download/refs/pingcap/tidb/${TIDB_BRANCH}/sha1").trim()
+                                    sh "curl ${FILE_SERVER_URL}/download/builds/pingcap/tidb/${tidb_sha1}/centos7/tidb-server.tar.gz | tar xz"
+                                    // tikv
+                                    def tikv_sha1 = sh(returnStdout: true, script: "curl ${FILE_SERVER_URL}/download/refs/pingcap/tikv/${TIKV_BRANCH}/sha1").trim()
+                                    sh "curl ${FILE_SERVER_URL}/download/builds/pingcap/tikv/${tikv_sha1}/centos7/tikv-server.tar.gz | tar xz"
+                                    // pd
+                                    def pd_sha1 = sh(returnStdout: true, script: "curl ${FILE_SERVER_URL}/download/refs/pingcap/pd/${PD_BRANCH}/sha1").trim()
+                                    sh "curl ${FILE_SERVER_URL}/download/builds/pingcap/pd/${pd_sha1}/centos7/pd-server.tar.gz | tar xz"
+                                    //ticdc
+                                    def ticdc_sha1 = sh(returnStdout: true, script: "curl ${FILE_SERVER_URL}/download/refs/pingcap/ticdc/${TICDC_BRANCH}/sha1").trim()
+                                    sh "curl ${FILE_SERVER_URL}/download/builds/pingcap/ticdc/${ticdc_sha1}/centos7/ticdc-linux-amd64.tar.gz | tar xz"
+                                    // kafka
+                                    sh "curl ${FILE_SERVER_URL}/download/${kafka_version}.tgz | tar xz"
+                                    sh "mv ${kafka_version} kafka/"
 
-                                // tidb
-                                def tidb_sha1 = sh(returnStdout: true, script: "curl ${FILE_SERVER_URL}/download/refs/pingcap/tidb/${TIDB_BRANCH}/sha1").trim()
-                                sh "curl ${FILE_SERVER_URL}/download/builds/pingcap/tidb/${tidb_sha1}/centos7/tidb-server.tar.gz | tar xz"
-                                // tikv
-                                def tikv_sha1 = sh(returnStdout: true, script: "curl ${FILE_SERVER_URL}/download/refs/pingcap/tikv/${TIKV_BRANCH}/sha1").trim()
-                                sh "curl ${FILE_SERVER_URL}/download/builds/pingcap/tikv/${tikv_sha1}/centos7/tikv-server.tar.gz | tar xz"
-                                // pd
-                                def pd_sha1 = sh(returnStdout: true, script: "curl ${FILE_SERVER_URL}/download/refs/pingcap/pd/${PD_BRANCH}/sha1").trim()
-                                sh "curl ${FILE_SERVER_URL}/download/builds/pingcap/pd/${pd_sha1}/centos7/pd-server.tar.gz | tar xz"
-                                //ticdc
-                                def ticdc_sha1 = sh(returnStdout: true, script: "curl ${FILE_SERVER_URL}/download/refs/pingcap/ticdc/${TICDC_BRANCH}/sha1").trim()
-                                sh "curl ${FILE_SERVER_URL}/download/builds/pingcap/ticdc/${ticdc_sha1}/centos7/ticdc-linux-amd64.tar.gz | tar xz"
-                                // kafka
-                                sh "curl ${FILE_SERVER_URL}/download/${kafka_version}.tgz | tar xz"
-                                sh "mv ${kafka_version} kafka/"
-
-                                sh """
+                                    sh """
                                         set -e
                                         set -x
                                         killall -9 tidb-server || true
@@ -134,7 +135,7 @@ def call(ghprbActualCommit, ghprbPullId, ghprbPullTitle, ghprbPullLink, ghprbPul
           
                                     """
 
-                                sh """
+                                    sh """
                                         rm -rf /tmp/zookeeper
                                         rm -rf /tmp/kafka-logs
                                         kafka/bin/zookeeper-server-start.sh kafka/config/zookeeper.properties &
@@ -145,24 +146,23 @@ def call(ghprbActualCommit, ghprbPullId, ghprbPullTitle, ghprbPullLink, ghprbPul
                                         kafka/bin/kafka-topics.sh --describe --topic tidb_test --bootstrap-server localhost:9092
                                     """
 
-                                sh """
+                                    sh """
                                         cd ticdc-linux-amd64
                                         ./bin/cdc server --pd="http://127.0.0.1:2379"  --log-file=ticdc.log --addr="0.0.0.0:8301" --advertise-addr="127.0.0.1:8301" &
                                         sleep 10
                                         ./bin/cdc cli changefeed create --pd="http://127.0.0.1:2379" --sink-uri="kafka://127.0.0.1:9092/tidb_test" --no-confirm
                                     """
-                            }
+                                }
 
-                            dir("/home/jenkins/agent/git/tibigdata") {
-                                try {
-                                    java_home = ""
+                                sh "pwd"
+                                java_home = ""
 
-                                    if (!isJava8) {
-                                        java_home = "export JAVA_HOME=/home/jenkins/agent/lib/jdk-11.0.12"
-                                    }
+                                if (!isJava8) {
+                                    java_home = "export JAVA_HOME=/home/jenkins/agent/lib/jdk-11.0.12"
+                                }
 
-                                    timeout(120) {
-                                        sh """
+                                timeout(120) {
+                                    sh """
                                         set -x
                                         set -euo pipefail
                                         export TIDB_HOST="127.0.0.1"
@@ -172,20 +172,20 @@ def call(ghprbActualCommit, ghprbPullId, ghprbPullTitle, ghprbPullLink, ghprbPul
                                         $java_home
                                         mvn clean test-compile failsafe:integration-test -am -pl ${module}
                                     """
-                                    }
-                                } catch (err) {
-                                    sh """
+                                }
+                            } catch (err) {
+                                sh """
                             ps aux | grep '-server' || true
                             curl -s 127.0.0.1:2379/pd/api/v1/status || true
                             """
-                                    sh "cat _run/pd.log"
-                                    sh "cat _run/tikv.log"
-                                    sh "cat _run/tidb.log"
-                                    sh "cat _run/kafka/logs/server.log"
-                                    sh "cat _run/ticdc-linux-amd64/ticdc.log"
-                                    throw err
-                                }
+                                sh "cat _run/pd.log"
+                                sh "cat _run/tikv.log"
+                                sh "cat _run/tidb.log"
+                                sh "cat _run/kafka/logs/server.log"
+                                sh "cat _run/ticdc-linux-amd64/ticdc.log"
+                                throw err
                             }
+
                         }
                     }
                 }
