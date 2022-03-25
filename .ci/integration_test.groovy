0b5ede2acdefb8ad745a9181a3288da4e1f29f5d
Leon Yang <yangxin@pingcap.com>
2022-03-25 11:52:56 +0800

    Update integration_test.groovy

--- .ci/integration_test.groovy
+++ .ci/integration_test.groovy
@@ -66,6 +66,7 @@ def call(ghprbActualCommit, ghprbPullId, ghprbPullTitle, ghprbPullLink, ghprbPul
                         curl -sL \$archive_url | tar -zx -C /maven
                         archive_url=http://fileserver.pingcap.net/download/builds/pingcap/tibigdata/cache/tibigdata-m2-cache-latest.tar.gz
                         curl -sL \$archive_url | tar -zx -C /maven
+                        ls /maven
                         """
                             if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
                                 deleteDir()
