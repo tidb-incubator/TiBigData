ef5aa79f446685e7241c6dd5e55a9bcbcccc1fc4
yangxin <yangxin@pingcap.com>
2022-05-17 15:50:18 +0800

    fix

--- .ci/build.groovy
+++ .ci/build.groovy
@@ -14,21 +14,23 @@ def call(ghprbActualCommit, ghprbPullId, ghprbPullTitle, ghprbPullLink, ghprbPul
                         archive_url=http://fileserver.pingcap.net/download/builds/pingcap/tibigdata/cache/tibigdata-m2-cache-latest.tar.gz
                         curl -sL \$archive_url | tar -zx -C /maven
                         
-                        mvn com.coveo:fmt-maven-plugin:format
-                        git diff --quiet
-                        formatted="\$?"
-                        if [[ "\${formatted}" -eq 1 ]]
-                        then
-                           echo "code format error, please run the following commands:"
-                           echo "mvn com.coveo:fmt-maven-plugin:format"
-                           exit 1
-                        fi
                         """
                         if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
                             deleteDir()
                         }
                         checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: credentialsId, refspec: '+refs/pull/*:refs/remotes/origin/pr/*', url: 'git@github.com:tidb-incubator/TiBigData.git']]]
                         sh "git checkout -f ${ghprbActualCommit}"
+                        sh """
+                        mvn com.coveo:fmt-maven-plugin:format
+                        git diff --quiet
+                        formatted="\\\$?"
+                        if [[ "\\${formatted}" -eq 1 ]]
+                        then
+                           echo "code format error, please run the following commands:"
+                           echo "mvn com.coveo:fmt-maven-plugin:format"
+                           exit 1
+                        fi
+                        """
                     }
                     dir("/home/jenkins/agent/lib") {
                         sh "curl https://download.pingcap.org/jdk-11.0.12_linux-x64_bin.tar.gz | tar xz"
