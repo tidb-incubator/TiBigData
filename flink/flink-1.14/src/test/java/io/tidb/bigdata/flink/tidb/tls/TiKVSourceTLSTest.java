2d25d190cca4ac20758c0f24ac33823575863373
Daemonxiao <735462752@qq.com>
2022-05-18 17:32:51 +0800

    Add TLS

---
+++ flink/flink-1.14/src/test/java/io/tidb/bigdata/flink/tidb/tls/TiKVSourceTLSTest.java
@@ -0,0 +1,87 @@
+package io.tidb.bigdata.flink.tidb.tls;
+
+import static io.tidb.bigdata.tidb.ClientConfig.*;
+import static java.lang.String.format;
+
+import io.tidb.bigdata.test.TableUtils;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableEnvironment;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TiKVSourceTLSTest {
+
+  public static final String tidbHost = "127.0.0.1";
+  public static final String tidbPort = "4000";
+  public static final String tidbUser = "root";
+  public static final String tidbPassword = "";
+
+  @Before
+  public void beforeTest() {
+    org.junit.Assume.assumeTrue(enableTLS());
+  }
+
+  public Boolean enableTLS() {
+    return Boolean.parseBoolean(System.getenv("TLS_ENABLE"));
+  }
+
+  @Test
+  public void testTLS() {
+    Map<String, String> properties = new HashMap<>();
+    properties.put(
+        DATABASE_URL,
+        format(
+            "jdbc:mysql://%s:%s/test?serverTimezone=Asia/Shanghai&zeroDateTimeBehavior=CONVERT_TO_NULL"
+                + "&tinyInt1isBit=false&enabledTLSProtocols=TLSv1,TLSv1.1,TLSv1.2"
+                + "&useSSL=true&requireSSL=true&verifyServerCertificate=false",
+            tidbHost, tidbPort));
+    properties.put(USERNAME, tidbUser);
+    properties.put(PASSWORD, tidbPassword);
+    properties.put(CLUSTER_TLS_ENABLE, "true");
+    properties.put(CLUSTER_TLS_CA, "/config/cert/root.crt");
+    properties.put(CLUSTER_TLS_CERT, "/config/cert/client.crt");
+    properties.put(CLUSTER_TLS_KEY, "/config/cert/client-pkcs8.key");
+    properties.put("type", "tidb");
+
+    TableEnvironment tableEnvironment = getTableEnvironment();
+    String createCatalogSql =
+        format("CREATE CATALOG `tidb` WITH ( %s )", TableUtils.toSqlProperties(properties));
+    tableEnvironment.executeSql(createCatalogSql);
+    Assert.assertFalse(tableEnvironment.getCatalog("tidb").get().listDatabases().isEmpty());
+  }
+
+  @Test
+  public void testJKS() {
+    Map<String, String> properties = new HashMap<>();
+    properties.put(
+        DATABASE_URL,
+        format(
+            "jdbc:mysql://%s:%s/test?serverTimezone=Asia/Shanghai&zeroDateTimeBehavior=CONVERT_TO_NULL"
+                + "&tinyInt1isBit=false&enabledTLSProtocols=TLSv1,TLSv1.1,TLSv1.2"
+                + "&useSSL=true&requireSSL=true&verifyServerCertificate=false",
+            tidbHost, tidbPort));
+    properties.put(USERNAME, tidbUser);
+    properties.put(PASSWORD, tidbPassword);
+    properties.put(CLUSTER_TLS_ENABLE, "true");
+    properties.put(CLUSTER_JKS_ENABLE, "true");
+    properties.put(CLUSTER_JKS_TRUST_PATH, "/config/cert/server-cert-store");
+    properties.put(CLUSTER_JKS_TRUST_PASSWORD, "12345678");
+    properties.put(CLUSTER_JKS_KEY_PATH, "/config/cert/client-keystore.p12");
+    properties.put(CLUSTER_JKS_KEY_PASSWORD, "123456");
+    properties.put("type", "tidb");
+
+    TableEnvironment tableEnvironment = getTableEnvironment();
+    String createCatalogSql =
+        format("CREATE CATALOG `tidb` WITH ( %s )", TableUtils.toSqlProperties(properties));
+    tableEnvironment.executeSql(createCatalogSql);
+    Assert.assertFalse(tableEnvironment.getCatalog("tidb").get().listDatabases().isEmpty());
+  }
+
+  protected TableEnvironment getTableEnvironment() {
+    EnvironmentSettings settings = EnvironmentSettings.newInstance().inBatchMode().build();
+    return TableEnvironment.create(settings);
+  }
+}
