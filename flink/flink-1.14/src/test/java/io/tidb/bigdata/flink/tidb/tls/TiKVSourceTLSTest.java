eb26ba9ada80b4704beddac3955d865434f495be
Daemonxiao <735462752@qq.com>
2022-05-30 19:11:16 +0800

    Update TLS

--- flink/flink-1.14/src/test/java/io/tidb/bigdata/flink/tidb/tls/TiKVSourceTLSTest.java
+++ flink/flink-1.14/src/test/java/io/tidb/bigdata/flink/tidb/tls/TiKVSourceTLSTest.java
@@ -41,9 +41,9 @@ public class TiKVSourceTLSTest {
     properties.put(USERNAME, tidbUser);
     properties.put(PASSWORD, tidbPassword);
     properties.put(CLUSTER_TLS_ENABLE, "true");
-    properties.put(CLUSTER_TLS_CA, "/config/cert/root.crt");
-    properties.put(CLUSTER_TLS_CERT, "/config/cert/client.crt");
-    properties.put(CLUSTER_TLS_KEY, "/config/cert/client-pkcs8.key");
+    properties.put(CLUSTER_TLS_CA, "/config/cert/pem/root.pem");
+    properties.put(CLUSTER_TLS_CERT, "/config/cert/pem/client.pem");
+    properties.put(CLUSTER_TLS_KEY, "/config/cert/pem/client-pkcs8.key");
     properties.put("type", "tidb");
 
     TableEnvironment tableEnvironment = getTableEnvironment();
@@ -67,9 +67,9 @@ public class TiKVSourceTLSTest {
     properties.put(PASSWORD, tidbPassword);
     properties.put(CLUSTER_TLS_ENABLE, "true");
     properties.put(CLUSTER_JKS_ENABLE, "true");
-    properties.put(CLUSTER_JKS_TRUST_PATH, "/config/cert/server-cert-store");
+    properties.put(CLUSTER_JKS_TRUST_PATH, "/config/cert/jks/server-cert-store");
     properties.put(CLUSTER_JKS_TRUST_PASSWORD, "12345678");
-    properties.put(CLUSTER_JKS_KEY_PATH, "/config/cert/client-keystore.p12");
+    properties.put(CLUSTER_JKS_KEY_PATH, "/config/cert/jks/client-keystore.p12");
     properties.put(CLUSTER_JKS_KEY_PASSWORD, "123456");
     properties.put("type", "tidb");
 
