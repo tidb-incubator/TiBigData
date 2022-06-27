4a3cef0299d77f73c2607168a77669e2e449f2f2
shiyuhang <1136742008@qq.com>
2022-06-27 13:12:16 +0800

    skip it in build

--- .ci/build.sh
+++ .ci/build.sh
@@ -6,18 +6,18 @@ set -euo pipefail
 # see maven plugin
 # git submodule update --init --recursive
 
-mvn clean compile test -B -am -pl ticdc -DskipIntegrationTests=true
-mvn clean compile test -B -am -pl flink/flink-1.11 -DskipIntegrationTests=true
-mvn clean compile test -B -am -pl flink/flink-1.12 -DskipIntegrationTests=true
-mvn clean compile test -B -am -pl flink/flink-1.13 -DskipIntegrationTests=true
-mvn clean compile test -B -am -pl flink/flink-1.14 -DskipIntegrationTests=true
-mvn clean compile test -B -am -pl mapreduce/mapreduce-base -DskipIntegrationTests=true
-mvn clean compile test -B -am -pl prestodb -DskipIntegrationTests=true
-mvn clean compile test -B -am -pl jdbc -DskipIntegrationTests=true
-mvn clean compile test -B -am -pl hive/hive-2.2.0 -DskipIntegrationTests=true
-mvn clean compile test -B -am -pl hive/hive-2.3.6 -DskipIntegrationTests=true
-mvn clean compile test -B -am -pl hive/hive-3.1.2 -DskipIntegrationTests=true
+mvn clean compile test -B -am -pl ticdc
+mvn clean compile test -B -am -pl flink/flink-1.11
+mvn clean compile test -B -am -pl flink/flink-1.12
+mvn clean compile test -B -am -pl flink/flink-1.13
+mvn clean compile test -B -am -pl flink/flink-1.14
+mvn clean compile test -B -am -pl mapreduce/mapreduce-base
+mvn clean compile test -B -am -pl prestodb
+mvn clean compile test -B -am -pl jdbc
+mvn clean compile test -B -am -pl hive/hive-2.2.0
+mvn clean compile test -B -am -pl hive/hive-2.3.6
+mvn clean compile test -B -am -pl hive/hive-3.1.2
 
 export JAVA_HOME=/home/jenkins/agent/lib/jdk-11.0.12
-mvn clean compile test -B -am -pl prestosql -DskipIntegrationTests=true
-mvn clean compile test -B -am -pl trino -DskipIntegrationTests=true
+mvn clean compile test -B -am -pl prestosql
+mvn clean compile test -B -am -pl trino
